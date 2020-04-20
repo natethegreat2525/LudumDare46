c29d9d698fc27dfb5a462f48828ee9834b51a1b2
Nathan Shirley <natethegreat2525@gmail.com>
2020-04-20 01:39:37 -0700

    Commit build

---
+++ build/precache-manifest.3ca288cf66828796e337d9db5e5767f0.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4c49f416c08d8fbb9aacabb82163ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e25dcb91d3dae8e84ec3",
+    "url": "/static/js/main.1462e75a.chunk.js"
+  },
+  {
+    "revision": "9d5d0b44ae8eabbc694b",
+    "url": "/static/js/runtime-main.806d7ab0.js"
+  }
+]);
